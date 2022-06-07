3e420153c5a823e7db3cdf139231d7d7e27fd2e0
billigproton <danrabien@seznam.cz>
2022-06-07 15:16:34 +0200

    Vylepšení kódu

---
+++ code.js
@@ -0,0 +1,21 @@
+﻿var skore = 0;
+    var polenaskore = document.getElementById("skore");
+    var vyhodnoceno = false;
+    function vyhodnotit(input){
+      if(input.className == "spravnaodpoved" && vyhodnoceno == false){
+        input.style.backgroundColor = "lightgreen";
+        skore += 100000;
+        vyhodnoceno = false;
+      } else if (input.className != "spravnaodpoved" && vyhodnoceno == false){
+        input.style.backgroundColor = "red";
+        skore = 0;
+        vyhodnoceno = true;
+        alert("špatně! Začněte znovu kliknutím na tlačítko Resetovat")
+      }
+      polenaskore.innerText = skore;
+    }
+    function resetovat(){
+      window.location.reload(true);
+    }
+
+  
